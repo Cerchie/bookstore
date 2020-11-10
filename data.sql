0d5a1f9ff9cd9d2d3dc1f4ed10d05c8617c764bc
Lucia Cerchie <luciacerchie@gmail.com>
2020-11-10 14:56:43 -0700

    tests

--- data.sql
+++ data.sql
@@ -1,3 +1,5 @@
+DROP TABLE IF EXISTS books;
+
 CREATE TABLE books (
   isbn TEXT PRIMARY KEY,
   amazon_url TEXT,
@@ -8,3 +10,14 @@ CREATE TABLE books (
   title TEXT, 
   year INTEGER
 );
+
+
+INSERT INTO books (isbn, amazon_url, author, language, pages, publisher, title, year)
+VALUES (
+978-3-16-148410-0,
+"http://a.co/eobPtX2",
+"Wilson Rawls",
+"English",
+"Penguin House",
+"Where the Red Fern Grows",
+1952);
